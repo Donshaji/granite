5065fea138ad14825ddaab833796badf5dcc98f9
Donshaji <donpattamana@gmail.com>
2024-09-01 16:38:52 +0530

    Added ability to update a task - sub

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  before_action :load_task!, only: %i[show update]
   def index
     tasks = Task.all
     render status: :ok, json: { tasks: }
   end
 
   def show
-    task = Task.find_by!(slug: params[:slug])
-    render_json({ task: })
+    render_json({ task: @task })
   end
 
   def create
@@ -17,9 +17,19 @@ class TasksController < ApplicationController
     render_notice(t("successfully_created"))
   end
 
+  def update
+    task = Task.find_by!(slug: params[:slug])
+    task.update!(task_params)
+    render_notice(t("successfully_updated"))
+  end
+
   private
 
     def task_params
       params.require(:task).permit(:title)
+    end
+
+    def load_task!
+      @task = Task.find_by!(slug: params[:slug])
     end
 end
--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -3,13 +3,15 @@ import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
+import { CreateTask, ShowTask, EditTask } from "components/Tasks";
+
 import Dashboard from "./components/Dashboard";
-import { CreateTask, ShowTask } from "./components/Tasks";
 
 const App = () => (
   <Router>
     <ToastContainer />
     <Switch>
+      <Route exact component={EditTask} path="/tasks/:slug/edit" />
       <Route exact component={ShowTask} path="/tasks/:slug/show" />
       <Route exact component={CreateTask} path="/tasks/create" />
       <Route exact component={Dashboard} path="/dashboard" />
